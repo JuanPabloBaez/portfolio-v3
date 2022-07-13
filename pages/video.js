6c066625bf9f79e3d47036e563619adf463dd970
JP Baez <baezjuanpablo@gmail.com>
2022-07-13 18:59:27 +0200

    web & video layout

--- pages/video.js
+++ pages/video.js
@@ -1,7 +1,25 @@
+import ReactPlayer from 'react-player';
 
 function Video() {
   return (
-    <h1>Video</h1>
+    <div className="video-body">
+           <p>The complete work is available on  <a href="http://beatkino.com/" target="_blank"  rel="noreferrer">beatkino.com</a></p>
+        
+            
+    
+            <ReactPlayer 
+            className="page-video"
+            url="https://vimeo.com/585754763"
+            controls={true}
+            responsive="true"
+            width={"100%"}
+            
+            
+            /> 
+                
+            
+
+        </div>
   )
 }
 
--- pages/web.js
+++ pages/web.js
@@ -1,7 +1,15 @@
 
 function Web() {
   return (
-    <h1>Web aqui</h1>
+    <div className="web-body">
+            
+    <span><p className="web-title">jpbaez.com</p> / <b>tech stack:</b> React, SaSS, Contentful(CMS)</span>
+    <span><a href="http://beatkino.com/" target="_blank"  rel="noreferrer">beatkino.com</a>  (film stream platform) / <b>tech stack:</b> React, Redux, SaSS, Node.js, MongoDB</span>
+    <span><a href="https://carloslertora.cl" target="_blank"  rel="noreferrer">carloslertora.cl</a>  (personal portfolio) / <b>tech stack:</b> React, CSS, Contentful (CMS)</span>
+    <span><a href="https://www.alejandroarevalo.cl/" target="_blank"  rel="noreferrer">alejandroarevalo.cl</a>  (personal portfolio) / <b>tech stack:</b> React, Recoil, SaSS, Contentful (CMS)</span>
+
+
+</div>
   )
 }
 
