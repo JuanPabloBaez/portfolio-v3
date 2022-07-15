2a4515f9d39c8d0cdbeae611d9c0810bca147891
JP Baez <baezjuanpablo@gmail.com>
2022-07-15 17:19:25 +0200

    image and bord layout

--- pages/board.js
+++ pages/board.js
@@ -1,3 +1,10 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import ReactPlayer from 'react-player';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+SwiperCore.use([Navigation]);
 
 
 export async function getStaticProps(){
@@ -14,10 +21,80 @@ export async function getStaticProps(){
   }
 }
 
-export default function Board(posts) {
+export default function Board({posts}) {
  
+  console.log(posts)
+
   return (
-    <h1>Board!</h1>
+    <div className="board-body">
+        <h1>Board</h1>
+        {posts.map((post, index)=> {
+            let{ header,
+                mainPicture,
+                date,
+                place,
+                placeLink,
+                mainContent1,
+                mainContent2,
+                videoLink,
+                media} = post.fields
+               
+            return(
+                <div className="post-container" key={index}>
+                    <h2>{header}</h2>
+                    <span>
+                        <p className="post-date">{date} </p>
+                        {(function() {
+                            if ( place && placeLink  ) {
+                                return <a className="post-place" href={placeLink} target="_blank" rel="noreferrer">on {place} </a>;  
+                            } if (place ==="") {
+                                return null
+                            } else {
+                                return <p className="post-place"> {place}</p>;
+                            }
+                        })()}
+                    </span>
+                    <div className="post-main-text">
+                        {mainPicture && <img className='main-photo' src={mainPicture.fields.file.url} alt={header}/>}
+                        <div className='post-main-content'>{documentToReactComponents(mainContent1)}</div>
+                        {/* <RichText 
+                            richText={mainContent1}
+                            overrides={{
+                                [BLOCKS.PARAGRAPH]: {
+                                    component: "p",
+                                    props: {
+                                    className: 'post-main-content'
+                                    }
+                                }
+                            }}
+                        /> */}
+                    </div>
+
+                    {media && media.length ===1 ? <img className="board-photo" src={post.fields.media[0].fields.file.url}  alt="post related"  />:null}
+                    {media && media.length > 1 ? <Swiper
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        height="100%"
+                        navigation
+                        loop={true}
+                    >
+                    { media.map((photo, index )=> {
+                        return (
+                            <SwiperSlide key={index}>
+                                <img className="board-photo" src={photo.fields.file.url}  alt="post related"  />
+                            </SwiperSlide>
+                        )
+                    })}  
+                    </Swiper>
+                    :null} 
+                    {mainContent2 ?  <div className='post-main-content2'>{documentToReactComponents(mainContent2)}</div> 
+                    : null}
+                    {videoLink && <ReactPlayer url={videoLink} className="board-player" width="100%" />}
+                    <hr></hr>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
--- pages/image.js
+++ pages/image.js
@@ -1,3 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+SwiperCore.use([Navigation]);
+
+
 export async function getStaticProps(){
 
   const client = require('contentful').createClient({
@@ -6,6 +15,7 @@ export async function getStaticProps(){
   })
   const res = await client.getEntries({content_type:'gallery'})
   const image = res.items
+ 
   return {
     props:{
       image
@@ -13,10 +23,49 @@ export async function getStaticProps(){
   }
 }
 
-function Image(image) {
-  
+function Image({image}) {
+   const [selector, setSelector] = useState("collage")
+   const [galleryItem, setGalleryItem] = useState([])
+
+  useEffect(()=>{
+    function selectImages (){
+      return Object.values(image).map(item => item.fields.title === selector ? setGalleryItem(item.fields.images):null )
+    }
+    selectImages();
+   },[selector])
+
+
+  function handleGallery () {
+    selector ==="collage" ? setSelector("photos") : setSelector("collage")
+  }
+
+
   return (
-    <h1>Image</h1>
+    <div className="gallery-body" >
+      <button onClick={handleGallery} >{selector==="collage" ? <p><b>Collage</b> / Photography</p>: <p>Collage / <b>Photography</b> </p>}</button>
+      
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        loop={true}
+        
+      >
+        {galleryItem.map((item,index)=>{
+          return(
+            <SwiperSlide key={index}>
+              <img className="slide-photo" src={item.fields.file.url}  alt={`gallery ${item}`} draggable={false}  />
+            </SwiperSlide>
+          )
+        })}
+        
+       
+        
+      </Swiper>
+    
+
+
+    </div>
   )
 }
 
