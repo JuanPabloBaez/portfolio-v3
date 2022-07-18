651b234e8467f2312d2c5fb67459b96743e89c27
JP Baez <baezjuanpablo@gmail.com>
2022-07-18 20:10:55 +0200

    favico fix

--- pages/web.js
+++ pages/web.js
@@ -7,9 +7,9 @@ function Web() {
         <title>JP Baez portfolio | Web Development &#38; Video Production </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         
-        <link rel="icon" href="../public/favicon.ico?" type="image/x-icon" />
-        <link rel="shortcut icon" href="../public/favicon.ico?" type="image/x-icon" />
-        <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png" /> 
+        <link rel="icon" href="/favicon.ico?" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico?" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" /> 
         <meta
             name="description"
             content="Portfolio Juan Pablo Baez. Web Development and Video Production. Based in Berlin, Germany."
@@ -18,14 +18,14 @@ function Web() {
         <meta property="og:type" content="website"/>
         <meta property="og:title" content="Juan Pablo Baez - Portfolio"/>
         <meta property="og:description" content="Personal portfolio of Juan Pablo Baez, frontend web developer (Javascript, React) and video producer."/>
-        <meta property="og:image" content="../public/metaLogo.jpg"/>
+        <meta property="og:image" content="/metaLogo.jpg"/>
 
         <meta name="twitter:card" content="summary_large_image"/>
         <meta property="twitter:domain" content="http://jpbaez.com/"/>
         <meta property="twitter:url" content="http://jpbaez.com/"/>
         <meta name="twitter:title" content="Juan Pablo Baez - Portfolio"/>
         <meta name="twitter:description" content="Personal portfolio of Juan Pablo Baez, frontend web developer (Javascript, React) and video producer."/>
-        <meta name="twitter:image" content="../public//metaLogo.jpg"/>
+        <meta name="twitter:image" content="/metaLogo.jpg"/>
     </Head>
     <div className="web-body">    
       <span><p className="web-title">jpbaez.com</p> / <b>tech stack:</b> React, SaSS, Contentful(CMS)</span>
